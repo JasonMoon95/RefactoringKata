ae4d8f692663c250e6fa821368045f0c241514bf
Seungje Mun <34952598+JasonMoon95@users.noreply.github.com>
2023-01-17 16:52:46 +0900

    Add Approval test

---
+++ Java/src/test/java/com/gildedrose/GildedRoseApprovalTest.java
@@ -0,0 +1,25 @@
+package com.gildedrose;
+
+import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class GildedRoseApprovalTest {
+    @Test
+    void updateQuality(){
+        String[] name = {"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert","Sulfuras, Hand of Ragnaros"};
+        Integer[] sellIn = {-1, 0, 6, 11};
+        Integer[] quality = {0, 2, 49, 50};
+
+        CombinationApprovals.verifyAllCombinations(this::doUpdateQuality, name, sellIn, quality);
+    }
+
+    private String doUpdateQuality(String name, int sellIn, int quality) {
+        GildedRose gildedRose = new GildedRose(new Item[]{Item.of(name, sellIn, quality)});
+        gildedRose.updateQuality();
+        String response = Arrays.asList(gildedRose.items).toString();
+        return response;
+    }
+}
