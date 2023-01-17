65e14fcd61cb43a94fcf1c958a4d36b894d0e950
Seungje Mun <34952598+JasonMoon95@users.noreply.github.com>
2023-01-17 17:10:51 +0900

    Add Approval test

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -19,7 +19,7 @@ class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        assertEquals("foo", items[0].name);
     }
 
     @Test
