3f4d80319d62939f97814c31441e07877871b348
Wooyoung Jung <wooyounj@amazon.com>
2023-01-19 16:37:17 +0900

    ueong's solution

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,69 +9,8 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            updateQualityFor(item);
+            item.updateQuality();
         }
     }
 
-    private static void updateQualityFor(Item item) {
-        if (isAgedBrie(item)) {
-            updateItemSellin(item);
-            updateAgedItemQuality(item);
-            if (item.sellIn < 0) {
-                updateAgedItemQuality(item);
-            }
-        } else if (isBackStage(item)) {
-            updateItemSellin(item);
-            updateAgedItemQuality(item);
-            if (item.sellIn < 10) {
-                updateAgedItemQuality(item);
-            }
-            if (item.sellIn < 5) {
-                updateAgedItemQuality(item);
-            }
-            if (item.sellIn < 0) {
-                updateItemQualityToZero(item);
-            }
-        } else if (isSulfuras(item)) {
-            // do nothing
-        } else {
-            updateItemSellin(item);
-            updateNormalItemQuality(item);
-            if (item.sellIn < 0) {
-                updateNormalItemQuality(item);
-            }
-        }
-    }
-
-    private static void updateItemQualityToZero(Item item) {
-        item.quality = 0;
-    }
-
-    private static void updateNormalItemQuality(Item item) {
-        if (item.quality > 0) {
-            item.quality = item.quality - 1;
-        }
-    }
-
-    private static void updateItemSellin(Item item) {
-        item.sellIn = item.sellIn - 1;
-    }
-
-    private static void updateAgedItemQuality(Item item) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
-        }
-    }
-
-    private static boolean isSulfuras(Item item) {
-        return item.name.equals("Sulfuras, Hand of Ragnaros");
-    }
-
-    private static boolean isBackStage(Item item) {
-        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
-    }
-
-    private static boolean isAgedBrie(Item item) {
-        return item.name.equals("Aged Brie");
-    }
 }
--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -3,9 +3,7 @@ package com.gildedrose;
 public class Item {
 
     public String name;
-
     public int sellIn;
-
     public int quality;
 
     public Item(String name, int sellIn, int quality) {
@@ -14,7 +12,81 @@ public class Item {
         this.quality = quality;
     }
 
-   @Override
+
+    public void updateQuality() {
+        if (isAgedBrie()) {
+            updateQualityForAgedBrie();
+        } else if (isBackStage()) {
+            updateQualityForBackStage();
+        } else if (isSulfuras()) {
+            // do nothing
+        } else {
+            updateQualityForNormal();
+        }
+    }
+    private void updateQualityForNormal() {
+        updateSellin();
+        updateNormalItemQuality();
+        if (sellIn < 0) {
+            updateNormalItemQuality();
+        }
+    }
+
+    private void updateQualityForBackStage() {
+        updateSellin();
+        updateAgedItemQuality();
+        if (sellIn < 10) {
+            updateAgedItemQuality();
+        }
+        if (sellIn < 5) {
+            updateAgedItemQuality();
+        }
+        if (sellIn < 0) {
+            updateQualityToZero();
+        }
+    }
+
+    private void updateQualityForAgedBrie() {
+        updateSellin();
+        updateAgedItemQuality();
+        if (sellIn < 0) {
+            updateAgedItemQuality();
+        }
+    }
+
+    private void updateQualityToZero() {
+        quality = 0;
+    }
+
+    private void updateNormalItemQuality() {
+        if (quality > 0) {
+            quality = quality - 1;
+        }
+    }
+
+    private void updateSellin() {
+        sellIn = sellIn - 1;
+    }
+
+    private void updateAgedItemQuality() {
+        if (quality < 50) {
+            quality = quality + 1;
+        }
+    }
+
+    private boolean isSulfuras() {
+        return name.equals("Sulfuras, Hand of Ragnaros");
+    }
+
+    private boolean isBackStage() {
+        return name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    private boolean isAgedBrie() {
+        return name.equals("Aged Brie");
+    }
+
+    @Override
    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
